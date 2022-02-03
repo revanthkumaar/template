bbee4a0d1370f6f0a23abe2387b8544a49b32b5b
revanthkumaar <iamrevanthkumar@gmail.com>
2022-02-03 13:59:48 +0530

    button format changed in guest onbaord and room tracker

--- src/layouts/roomAvailibity/index.js
+++ src/layouts/roomAvailibity/index.js
@@ -17,8 +17,8 @@ function RoomAvailability() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <MDBox py={1}>
-        <MDBox mt={4.5}>
+      <MDBox py={1} bgColor="white">
+        <MDBox>
           <Grid container spacing={6}>
             <Grid item xs={20} md={10} lg={20}>
             <Main display="flex" justifyContent="flex-end" />
