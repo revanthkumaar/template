755b28749be071fcd5cc706f85edcd28668d1f0b
manohar519 <77038754+manohar519@users.noreply.github.com>
2022-02-03 09:59:37 +0530

    Updated the bugs

--- src/layouts/profile/GuestLoginForm/components/Payments.js
+++ src/layouts/profile/GuestLoginForm/components/Payments.js
@@ -51,10 +51,16 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: '18,000', firstName: 'B101', age: "Yes/No",bed_id:"G12",shelf:3,monthlyrent:"20000",dailyrent:800},
-  { id: 2, lastName: '10,000', firstName: 'A101', age: "Yes/No",bed_id:"P1C",shelf:4 ,monthlyrent:"11500",dailyrent:600},
-  { id: 3, lastName: '7,500', firstName: 'C102', age: "Yes/No",bed_id:"G1A" ,shelf:5,monthlyrent:"8500",dailyrent:500},
-  { id: 4, lastName: '6,500', firstName: 'D100', age: "Yes/No",bed_id:"B1A",shelf:1,monthlyrent:"7500",dailyrent:400},
+  { id: 1, lastName: '18,000', firstName: 'B101', age: "Yes",bed_id:"G12",shelf:3,monthlyrent:"20000",dailyrent:800},
+  { id: 2, lastName: '10,000', firstName: 'A101', age: "Yes",bed_id:"P1C",shelf:4 ,monthlyrent:"11500",dailyrent:600},
+  { id: 3, lastName: '7,500', firstName: 'C102', age: "Yes",bed_id:"G1A" ,shelf:5,monthlyrent:"8500",dailyrent:500},
+  { id: 4, lastName: '6,500', firstName: 'D100', age: "No",bed_id:"B1A",shelf:1,monthlyrent:"7500",dailyrent:400},
+  { id: 5, lastName: '7,500', firstName: 'C101', age: "Yes",bed_id:"B2A",shelf:2,monthlyrent:"8500",dailyrent:500},
+  { id: 6, lastName: '6,500', firstName: 'A101', age: "No",bed_id:"B2A",shelf:3,monthlyrent:"7500",dailyrent:400},
+  { id: 7, lastName: '6,500', firstName: 'P101', age: "Yes",bed_id:"B2B",shelf:4,monthlyrent:"8500",dailyrent:600},
+  { id: 8, lastName: '6,500', firstName: 'G101', age: "No",bed_id:"B22",shelf:1,monthlyrent:"7500",dailyrent:400},
+  { id: 9, lastName: '6,500', firstName: 'F101', age: "Yes",bed_id:"D2A",shelf:2,monthlyrent:"6500",dailyrent:500},
+
 ]
 
 export default function DataGridDemo() {
