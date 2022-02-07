b9ef4966178c03973901d3ac6c34d886d6825189
MRDJsekhar <saisekhar77492@gmail.com>
2022-02-07 17:42:08 +0530

    changed color to  table head and reduced column size of tables in dashboard

--- src/layouts/dashboard/components/summaryTables/AvailableRooms.js
+++ src/layouts/dashboard/components/summaryTables/AvailableRooms.js
@@ -2,8 +2,7 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-	{ field: 'id', headerName: 'SLNO', width: 70 },
-	{ field: 'bedId', headerName: 'BED ID', width: 70 },
+	{ field: 'id', headerName: 'BED ID', width: 80 },
 	{ field: 'buildiing', headerName: 'Building', width: 130 },
 	{ field: 'floor', headerName: 'Floor', width: 130 },
 	{ field: 'roomNumber', headerName: 'Room Number', width: 130 },
@@ -12,40 +11,35 @@ const columns = [
 
 const rows = [
 	{
-		id: 1,
-		bedId: '203A',
+		id: '203A',
 		buildiing: 'sharan PG',
 		floor: 'First Floor',
 		roomNumber: '001',
 		price: 6000
 	},
 	{
-		id: 2,
-		bedId: '307A',
+		id: '307A',
 		buildiing: 'sharan PG',
 		floor: 'First Floor',
 		roomNumber: '001',
 		price: 5500
 	},
 	{
-		id: 3,
-		bedId: '206B',
+		id: '206B',
 		buildiing: 'sharan PG',
 		floor: 'Second Floor',
 		roomNumber: '002',
 		price: 4000
 	},
 	{
-		id: 4,
-		bedId: '108A',
+		id: '108A',
 		buildiing: 'sharan PG',
 		floor: 'Second Floor',
 		roomNumber: '003',
 		price: 5000
 	},
 	{
-		id: 5,
-		bedId: '109C',
+		id: '109C',
 		buildiing: 'sharan PG',
 		floor: 'Ground Floor',
 		roomNumber: '004',
