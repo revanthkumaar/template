e2ef4d8bd82e2e50b334ab83e8c49abbe9e2fe64
vineeth-kumars <sattinenivineethkumar@gmail.com>
2022-02-22 10:53:29 +0530

    integrated charts in RAT and guestimages in data and RAT

--- src/layouts/roomavailabilitytracker/components/Layout.js
+++ src/layouts/roomavailabilitytracker/components/Layout.js
@@ -3,7 +3,7 @@ import { Grid } from '@mui/material'
 import MDBox from 'components/MDBox'
 import allBuildingsData from './Data';
 import HotelOutlinedIcon from '@mui/icons-material/HotelOutlined';
-
+import IconButton from '@material-ui/core/IconButton';
 import { pink } from '@mui/material/colors';
 import MDTypography from 'components/MDTypography';
 
@@ -31,11 +31,19 @@ const Layout = () => {
                                 <>
                                   <Grid item xs >
                                     <Grid container Spacing={1} direction="column" alignItems="center" justifyContent="center" >
-                                      <Grid item xs={12}>
-                                        {bdno.available ? <HotelOutlinedIcon color="success" />
-                                          : <HotelOutlinedIcon sx={{ color: pink[500] }} />}
-                                        <Grid item xs={12} ><h6 align="left" >{bdno.bedNumber}</h6></Grid>
-                                      </Grid>
+                                      <Grid item xs={12} align="center" >
+                                      {/* <img src={bdno.guestimageUrl} style={{width: '5px'}}/>  */}
+                                      {/* {bdno.available ? <img src={bdno.guestimageUrl} style={{width: '30px',height:'30px'}}/> : <IconButton style={{width: '50px' ,height:'30px'}}><HotelOutlinedIcon sx={{ color: pink[500] }} /></IconButton>}  */}
+                                         {/* <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" /> */}
+
+                                         {bdno.available ? <IconButton><HotelOutlinedIcon color="success" /></IconButton>
+                                          :  <img src={bdno.guestimageUrl} style={{width: '30px',height:'30px'}}/>}
+                                      
+                                        {/* {bdno.available ? <Image src={bdno.guestimageUrl} />:<IconButton><HotelOutlinedIcon sx={{ color: pink[500] }} /></IconButton>}} */}
+                                        
+                                        
+                                        <Grid item xs={12}  align="center" ><h6 align="center"  >{bdno.bedNumber}</h6></Grid>
+                                        </Grid>
                                     </Grid>
                                   </Grid>
                                 </>
