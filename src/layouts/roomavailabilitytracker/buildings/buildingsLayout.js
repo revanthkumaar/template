4933daecd79475b57aaa88f80aa18655cb828ffe
vineeth-kumars <sattinenivineethkumar@gmail.com>
2022-03-14 10:57:30 +0530

    changes in RAT added PopUp

--- src/layouts/roomavailabilitytracker/buildings/buildingsLayout.js
+++ src/layouts/roomavailabilitytracker/buildings/buildingsLayout.js
@@ -15,18 +15,13 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import MDButton from 'components/MDButton';
 import axios from 'axios';
-import GuestPopUp from './dialogbox';
+import GuestPopUp from './guestPopUP';
 import { setOpenConfigurator } from 'context';
 
 const BuildingsLayout = (props) => {
+    
     const [buildingInfo, setBuildingInfo] = React.useState({});
     const [Floors, setFloors] = React.useState([]);
-
-    const [open, setOpen] = React.useState(false);
-         const handleOpen = () => setOpen(true);
-        const handleClose = () => setOpen(false);
-   
-
     useEffect(() => {
         const GetData = async () => {
             const url = "http://localhost:8084/bed/getBedsByBuildingId/1";
@@ -35,8 +30,6 @@ const BuildingsLayout = (props) => {
                 const build = await resp.json();
                 setBuildingInfo(build);
                 setFloors(build.floors)
-
-
             }
             catch (err) {
                 console.error(err);
@@ -45,130 +38,42 @@ const BuildingsLayout = (props) => {
         GetData();
     }, []);
 
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => setOpen(false);
 
 
     return (
         <>
-
             <MDBox bgColor="white" padding="30px" sx={{ border: 3 }} >
-                <Grid container  spacing={2} >
+                <Grid container spacing={2} >
                     <Grid item xs={12}>
                         <h4>{buildingInfo.buildingName}</h4>
                     </Grid>
-
                     {Floors.map(item => {
                         return (
                             <>
                                 <Grid item xs={12}>
                                     <h6 align="center">{item.floorName}</h6></Grid>
-
                                 {item.rooms.map(rm => {
                                     return (
                                         <>
                                             <Grid item xs={3}>
                                                 <Grid spacing={-3} container rowSpacing={1} sx={{ border: 1 }}>
                                                     <Grid item xs={12}>{rm.roomNumber}</Grid>
-
                                                     {rm.beds.map(bd => {
                                                         return (
                                                             <>
                                                                 <Grid item xs >
-                                                                <Grid container Spacing={1} direction="column" alignItems="center" justifyContent="center"  >
-                                                                    <Grid item xs={12} align='center'>  {bd.available ?
+                                                                    <Grid container Spacing={1} direction="column" alignItems="center" justifyContent="center"  >
+                                                                        <Grid item xs={12} align='center'>  {bd.available ?
                                                                             <IconButton><HotelOutlinedIcon color='success' /></IconButton>
                                                                             : <>
-                                                                                <IconButton onClick={()=>{setOpen(true)}}><HotelOutlinedIcon color='error' /></IconButton>
-                                                                                <GuestPopUp open={open} handleClose={handleClose}/>
-                                                                                {/* <Dialog open={open} onClose={handleClose} maxWidth='lg'>
-                <DialogTitle>GUEST DETAILS</DialogTitle>
-                <br />
-                <DialogContent>
-                    <Grid container spacing={1}>
-                        <Grid item xs={4} >
-                            <Grid container spacing={1}>
-                                <Grid item xs={12}>
-                                    <MDTypography
-                                     id="outlined-textarea"
-                                    //   label="Guest Id"
-                                      style={{ width: '65%', marginLeft: '50px', marginBottom: '20px', }}
-                                      > Name :</MDTypography> 
-                                </Grid>
-                            <Grid item xs={12}>
-                                <MDTypography
-                                id="outlined-textarea"
-                                label="Amount"
-                                style={{ width: '65%', marginLeft: '50px', marginBottom: '20px' }}
-                            >Building  :</MDTypography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MDTypography
-                                id="outlined-textarea"
-                                label="Payment Id"
-                                style={{ width: '65%', marginLeft: '50px', marginBottom: '20px' }}
-                            >Aadhar  :</MDTypography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MDTypography
-                                id="outlined-textarea"
-                                label="Transaction Date"
-                                style={{ width: '65%', marginLeft: '50px', marginBottom: '20px' }}
-                            >Parents phone  :</MDTypography>
-                            </Grid>
-
-                            </Grid>
-                            </Grid>
-
-                            <Grid item xs={4} >
-                            <Grid container spacing={1}>
-                                <Grid item xs={12}>
-                                    <MDTypography
-                                     id="outlined-textarea"
-                                      label="Guest Id"
-                                     style={{ width: '65%', marginLeft: '50px', marginBottom: '20px', }}
-                                      > Room NO :</MDTypography> 
-                                </Grid>
-                            <Grid item xs={12}>
-                                <MDTypography
-                                id="outlined-textarea"
-                                label="Amount"
-                                style={{ width: '65%', marginLeft: '50px', marginBottom: '20px' }}
-                            >Phone  :</MDTypography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MDTypography
-                                id="outlined-textarea"
-                                label="Payment Id"
-                                style={{ width: '65%', marginLeft: '50px', marginBottom: '20px' }}
-                            >Email Id  :</MDTypography>
-                            </Grid>
-                            
-
-                            </Grid>
-                            </Grid>
-                            <Grid item xs={4} sx={{ border: 1 }}><div>
-                                                                      </div></Grid>
-                        </Grid>
-
-                        
-                </DialogContent>
-                <DialogActions>
-                    <Grid container alignItems="center" alignContent="center">
-                        <MDButton
-                            width='20%'
-                            variant="contained"
-                            color="primary" size="large"
-                            justify="center"
-                            style={{ borderRadius: 10 }}
-                            onClick={handleClose}>Close</MDButton>
-                    </Grid>
-
-                </DialogActions>
-            </Dialog> */}
-
+                                                                                <IconButton onClick={() => { setOpen(true) }}><HotelOutlinedIcon color='error' /></IconButton>
+                                                                                <GuestPopUp open={open} handleClose={handleClose} />
                                                                             </>}
-                                                                             </Grid>
+                                                                        </Grid>
                                                                         <Grid item xs={12}> <h6 align='center'>{bd.bedNumber}</h6></Grid>
-                                                                   </Grid>
+                                                                    </Grid>
                                                                 </Grid>
                                                             </>
                                                         )
