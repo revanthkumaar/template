bf7c4fc58e9b957d6975930885e907367372afdc
SrivedaAnnaparthi <sriveda.annaparthi@arshaa.com>
2022-03-21 17:57:56 +0530

    changes in RAT

--- src/layouts/roomavailabilitytracker/BuildingDropdown.js
+++ src/layouts/roomavailabilitytracker/BuildingDropdown.js
@@ -24,14 +24,14 @@ function BuildingDropdown() {
 				style={{ width: '30%', height: '10%' }}
 				value={selected}
 				onChange={handleChange}
-				name="country"
-				displayEmpty
+				name="building"
+				
 				
 			>
 				
-				<MenuItem value="Building-1" component={Link} to ='/layouts/roomavailabilitytracker/buildings/SreeKalaNilayam'>SREE KALA NILAYAM</MenuItem>
+				<MenuItem value="SREE KALA NILAYAM" component={Link} to ='/layouts/roomavailabilitytracker/buildings/SreeKalaNilayam'>SREE KALA NILAYAM</MenuItem>
 				<MenuItem value="ANANDANILAYAM" component={Link} to ='/layouts/roomavailabilitytracker/buildings/AnandaNilayam'>ANANDA NILAYAM</MenuItem>
-				<MenuItem value="Building-3" component={Link} to = '/layouts/roomavailabilitytracker/buildings/SreeNilayam'>SREE NILAYAM</MenuItem>
+				<MenuItem value="SREE NILAYAM" component={Link} to = '/layouts/roomavailabilitytracker/buildings/SreeNilayam'>SREE NILAYAM</MenuItem>
 				{/* <MenuItem value="Building-4"   component={Link} to = "/layouts/roomavailabilitytracker/buildings/buildingFour">Building-4</MenuItem> */}
 			</Select>
 		</div>
