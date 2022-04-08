f6f63274115c68db72d333b4ac03c57387ea96af
sandhya0742 <sandhyabandaru15@gmail.com>
2022-04-08 10:21:44 +0530

    changes made buildings table

--- src/layouts/branch/components/Actions.js
+++ src/layouts/branch/components/Actions.js
@@ -1,216 +1,226 @@
-import React, { useState } from "react";
-
+import React, { useState ,useEffect} from "react";
 import MaterialTable from "material-table";
-import { Grid ,Box} from "@mui/material";
+import axios from "axios";
+import { Grid } from "@mui/material"
 
-const empList = [
-  {
-    id: 1,
-    buildingName: "Building-1",
-    buildingNumber: "A12",
-    managerName: "Sivayya",
-    mobile: 9876543210,
-  },
-  {
-    id: 2,
-    buildingName: "Building-2",
-    buildingNumber: "B11",
-    managerName: "Subramanyam",
-    mobile: 9812345678,
-  },
-  {
-    id: 3,
-    buildingName: "Building-3",
-    buildingNumber: "C03",
-    managerName: "HariBabu",
-    mobile: 7896536289,
-  },
-  {
-    id: 4,
-    buildingName: "Building-4",
-    buildingNumber: "B11",
-    managerName: "Suresh",
-    mobile: 9087654321,
-  },
-  {
-    id: 5,
-    buildingName: "Building-1",
-    buildingNumber: "A12",
-    managerName: "Sivayya",
-    mobile: 9876543210,
-  },
-  {
-    id: 6,
-    buildingName: "Building-2",
-    buildingNumber: "B11",
-    managerName: "Subramanyam",
-    mobile: 9812345678,
-  },
-  {
-    id: 7,
-    buildingName: "Building-3",
-    buildingNumber: "C03",
-    managerName: "HariBabu",
-    mobile: 7896536289,
-  },
-  {
-    id: 8,
-    buildingName: "Building-4",
-    buildingNumber: "B11",
-    managerName: "Suresh",
-    mobile: 9087654321,
-  },
-  {
-    id: 9,
-    buildingName: "Building-1",
-    buildingNumber: "A12",
-    managerName: "Sivayya",
-    mobile: 9876543210,
-  },
-  {
-    id: 10,
-    buildingName: "Building-2",
-    buildingNumber: "B11",
-    managerName: "Subramanyam",
-    mobile: 9812345678,
-  },
-  {
-    id: 11,
-    buildingName: "Building-3",
-    buildingNumber: "C03",
-    managerName: "HariBabu",
-    mobile: 7896536289,
-  },
-  {
-    id: 12,
-    buildingName: "Building-4",
-    buildingNumber: "B11",
-    managerName: "Suresh",
-    mobile: 9087654321,
-  },
-];
+
+// const empList = [
+//   {
+//     id: 1,
+//     buildingName: "Building-1",
+//     buildingNumber: "A12",
+//     managerName: "Sivayya",
+//     mobile: 9876543210,
+//   },
+//   {
+//     id: 2,
+//     buildingName: "Building-2",
+//     buildingNumber: "B11",
+//     managerName: "Subramanyam",
+//     mobile: 9812345678,
+//   },
+//   {
+//     id: 3,
+//     buildingName: "Building-3",
+//     buildingNumber: "C03",
+//     managerName: "HariBabu",
+//     mobile: 7896536289,
+//   },
+//   {
+//     id: 4,
+//     buildingName: "Building-4",
+//     buildingNumber: "B11",
+//     managerName: "Suresh",
+//     mobile: 9087654321,
+//   },
+//   {
+//     id: 5,
+//     buildingName: "Building-1",
+//     buildingNumber: "A12",
+//     managerName: "Sivayya",
+//     mobile: 9876543210,
+//   },
+//   {
+//     id: 6,
+//     buildingName: "Building-2",
+//     buildingNumber: "B11",
+//     managerName: "Subramanyam",
+//     mobile: 9812345678,
+//   },
+//   {
+//     id: 7,
+//     buildingName: "Building-3",
+//     buildingNumber: "C03",
+//     managerName: "HariBabu",
+//     mobile: 7896536289,
+//   },
+//   {
+//     id: 8,
+//     buildingName: "Building-4",
+//     buildingNumber: "B11",
+//     managerName: "Suresh",
+//     mobile: 9087654321,
+//   },
+//   {
+//     id: 9,
+//     buildingName: "Building-1",
+//     buildingNumber: "A12",
+//     managerName: "Sivayya",
+//     mobile: 9876543210,
+//   },
+//   {
+//     id: 10,
+//     buildingName: "Building-2",
+//     buildingNumber: "B11",
+//     managerName: "Subramanyam",
+//     mobile: 9812345678,
+//   },
+//   {
+//     id: 11,
+//     buildingName: "Building-3",
+//     buildingNumber: "C03",
+//     managerName: "HariBabu",
+//     mobile: 7896536289,
+//   },
+//   {
+//     id: 12,
+//     buildingName: "Building-4",
+//     buildingNumber: "B11",
+//     managerName: "Suresh",
+//     mobile: 9087654321,
+//   },
+// ];
 
 function Actions() {
-  const [data, setData] = useState(empList);
+  const [data, setData] = useState([]);
   const columns = [
     {
-      title: "ID",
-      field: "id",
-      editable: false,
-      width: "4%",
-      headerStyle: {
-        backgroundColor: "#1E90FF",
-        color: "white",
-        mt: 2,
-        mb: 2,
-        height: 10,
-      },
-    },
-    {
       title: "Building Name",
-      field: "buildingName",
+      field: "building_name",
       headerStyle: {
         backgroundColor: "#1E90FF",
         color: "white",
-        height: 10,
-      },
-    },
-    {
-      title: "Building Number",
-      field: "buildingNumber",
-      headerStyle: {
-        backgroundColor: "#1E90FF",
-        color: "white",
-        height: 10,
       },
     },
     {
       title: "Manager Name",
-      field: "managerName",
+      field: "manager_name",
       headerStyle: {
         backgroundColor: "#1E90FF",
         color: "white",
-        height: 10,
       },
     },
     {
       title: "Phone Number",
-      field: "mobile",
+      field: "phone_number",
       headerStyle: {
         backgroundColor: "#1E90FF",
         color: "white",
-        height: 10,
       },
     },
+
   ];
+
+  useEffect(() => {
+    axios
+
+      .get("http://localhost:8085/bed/getAllBuildings")
+
+      .then((res) => {
+        setData(res.data);
+
+        console.log(res.data);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     
       
-      <Grid container>
+    
+    
+    <Grid container>
         <Grid xs={30}>
-          <MaterialTable
-            title="Manage Employees"
-            data={data}
-            sx={{ color: "white" }}
-            columns={columns}
-            editable={{
-              onRowAdd: (newRow) =>
-                new Promise((resolve, reject) => {
-                  const updatedRows = [
-                    ...data,
-                    { id: Math.floor(Math.random() * 100), ...newRow },
-                  ];
+      
+        <MaterialTable
+          title="Manage Employees"
+          data={data}
+          sx={{ color: "white" }}
+          columns={columns}
+          editable={{
+            onRowAdd: (newRow) =>
+              new Promise((resolve, reject) => {
+                const updatedRows = [
+                  ...data,
+                  { id: Math.floor(Math.random() * 100), ...newRow },
+                ];
+                setTimeout(() => {
+                  const res = axios.post(
+                    "http://localhost:8085/bed/addBuilding",
+
+                    newRow
+                  );
                   console.log(newRow);
-                  setTimeout(() => {
-                    setData(updatedRows);
-                    resolve();
-                  }, 2000);
+                  setData(updatedRows);
+                  resolve();
+                }, 2000);
+              }),
+            onRowDelete: (selectedRow) =>
+              new Promise((resolve, reject) => {
+                const index = selectedRow.building_id;
+                const updatedRows = [...data];
+                updatedRows.splice(index, 1);
+                setTimeout(() => {
+                  const res = axios.delete(
+                    `http://localhost:8085/bed/deleteBuilding/${index}`
+                  );
+                  console.log(res);
                   console.log(updatedRows);
-                }),
-              onRowDelete: (selectedRow) =>
-                new Promise((resolve, reject) => {
-                  const index = selectedRow.tableData.id;
-                  const updatedRows = [...data];
-                  updatedRows.splice(index, 1);
-                  setTimeout(() => {
-                    setData(updatedRows);
-                    resolve();
-                  }, 2000);
-                }),
-              onRowUpdate: (updatedRow, oldRow) =>
-                new Promise((resolve, reject) => {
-                  const index = oldRow.tableData.id;
-                  const updatedRows = [...data];
-                  updatedRows[index] = updatedRow;
-                  setTimeout(() => {
-                    setData(updatedRows);
-                    resolve();
-                  }, 2000);
-                }),
-            }}
-            options={{
-              exportButton: true,
-              grouping: true,
-              actionsColumnIndex: -1,
-              addRowPosition: "first",
-              headerStyle: {
-                backgroundColor: "#1E90FF",
-                color: "white",
-                fontSize: "20px",
-                fontWeight: "bold",
-                height: 10,
-              },
-              rowStyle: {
-                fontSize: 16,
-              },
-            }}
-          />
-        </Grid>
+                  setData(updatedRows);
+                  resolve();
+                }, 2000);
+              }),
+            onRowUpdate: (updatedRow, oldRow) =>
+              new Promise((resolve, reject) => {
+                const index = oldRow.building_id;
+                const updatedRows = [...data];
+                updatedRows[index] = updatedRow;
+                setTimeout(() => {
+                  const res = axios.put(
+                    `http://localhost:8085/bed/updateBuildingById/${index}`,
+                    updatedRow
+                  );
+
+                  console.log(updatedRows);
+                  setData(updatedRows);
+                  resolve();
+                }, 2000);
+              }),
+          }}
+          options={{
+            exportButton:true,
+            grouping:true,
+            actionsColumnIndex: -1,
+            addRowPosition: "first",
+            headerStyle: {
+              backgroundColor: "#1E90FF",
+              color: "white",
+              fontSize: "15px",
+              height: "10px",
+              fontWeight: 'bold'
+            },
+            rowStyle: {
+              fontSize: 16,
+            },
+          }}
+        />
+      </Grid>
       </Grid>
     
-  
-  );
+ 
+);
 }
-
 export default Actions;
