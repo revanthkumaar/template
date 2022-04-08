497484b27332c5c43193592974525b26bf685134
SrivedaAnnaparthi <sriveda.annaparthi@arshaa.com>
2022-04-08 16:46:58 +0530

    Recent transactions made dynamic

--- src/layouts/dashboard/components/summaryTables/Recenttransactions.js
+++ src/layouts/dashboard/components/summaryTables/Recenttransactions.js
@@ -1,74 +1,84 @@
-import * as React from 'react';
+import  React, {useEffect,useState} from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+
+// "id": 0,
+//         "amountPaid": 6000.0,
+//         "transactionId": "lkjhgfd",
+//         "transactionDate": "2022-04-08",
+//         "paymentPurpose": "roomrent",
+//         "guestId": "SLH000002"
 
 const columns = [
-	{ field: 'id', headerName: 'BED ID', width: 80 },
-	{ field: 'name', headerName: 'NAME', width: 150 },
-	{
-		field: 'doj',
-		headerName: 'DOJ',
-		width: 90
-	},
-	{ field: 'rent', headerName: 'RENT', width: 80 },
-	{ field: 'received', headerName: 'RECEIVED', width: 130 }
+	{ field: 'id', headerName: ' PAYMENT ID', width: 80 },
+	{ field: 'amountPaid', headerName: 'AMOUNT PAID', width: 130 },
+	{ field: 'transactionId', headerName: 'TRANSACTION ID', width: 80 },
+	{ field: 'transactionDate', headerName: 'TRANSACTION DATE', width: 40 },
+	{ field: 'paymentPurpose', headerName: 'PAYMENT PURPOSE', width: 80 },
+	{ field: 'guestId', headerName: 'GUEST ID', width: 80 },
+	
+
+
+
 ];
 
-const rows = [
-	{
-		id: '203A',
-		name: 'Vinay Mhatre',
-		doj: '01-oct-2021',
-		rent: '10000',
-		received: '10000',
-	},
-	{
-		id: '307A',
-		name: 'Om Pandey',
-		doj: '26-July-21',
-		rent: '7000',
-		received: '7000',
-	},
-	{
-		id: '206B',
-		name: 'Nelson George',
-		doj: '31-July-21',
-		rent: '7500',
-		received: '7500',
-	},
-	{
-		id: '108A',
-		name: 'V. Charan',
-		doj: '10-July-21',
-		rent: '8500',
-		received: '8500',
-	},
-	{
-		id: '109C',
-		name: 'Kishore Reddy',
-		doj: '30-Sep-21',
-		rent: '9000',
-		received: '9000',
-	},
-	{
-		id: '305C',
-		name: 'Ashish Onkala',
-		doj: '02-Oct-21',
-		rent: '7500',
-		received: '5000',
-	},
-	{
-		id: 'P3A',
-		name: 'A.Shanmukh Rao',
-		doj: '04-Jul-21',
-		rent: '6500',
-		received: '6500',
-	}
-];
 
-export default function RecentTransactions() {
+
+export default function AvailableRooms() {
+	// const rows = [
+	// 	{
+	// 		id: 'table',
+	// 		buildiing: 'sharan PG',
+	// 		floor: 'First Floor',
+	// 		roomNumber: '001',
+	// 		price: 6000
+	// 	},
+	// 	{
+	// 		id: '307A',
+	// 		buildiing: 'sharan PG',
+	// 		floor: 'First Floor',
+	// 		roomNumber: '001',
+	// 		price: 5500
+	// 	},
+	// 	{
+	// 		id: '206B',
+	// 		buildiing: 'sharan PG',
+	// 		floor: 'Second Floor',
+	// 		roomNumber: '002',
+	// 		price: 4000
+	// 	},
+	// 	{
+	// 		id: '108A',
+	// 		buildiing: 'sharan PG',
+	// 		floor: 'Second Floor',
+	// 		roomNumber: '003',
+	// 		price: 5000
+	// 	},
+	// 	{
+	// 		id: '109C',
+	// 		buildiing: 'sharan PG',
+	// 		floor: 'Ground Floor',
+	// 		roomNumber: '004',
+	// 		price: 5000
+	// 	}
+	// ];
+	const [transaction,setTransaction]=React.useState([])
+
+	useEffect(() => {
+		axios
+		  .get("http://localhost:8989/payment/getRecentPayments?field=date")
+		  .then((res) => {
+			setTransaction(res.data);
+			console.log(res.data);
+		  })
+	
+		  .catch((err) => {
+			console.log(err);
+		  });
+	  }, []);
 	return (
 		<div style={{ height: 400, width: '100%' }}>
-			<DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[ 5 ]} />
+			<DataGrid rows={transaction} columns={columns} pageSize={5} rowsPerPageOptions={[ 5 ]} />
 		</div>
 	);
 }
