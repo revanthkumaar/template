5da90ecf1df1f0cd1f0ba2523240d7ce348be3c3
vineeth-kumars <sattinenivineethkumar@gmail.com>
2022-04-19 19:03:30 +0530

    paymentSummary chart made dynamic

--- src/layouts/roomavailabilitytracker/buildings/buildingspieCharts/paymentSummaryChart.js
+++ src/layouts/roomavailabilitytracker/buildings/buildingspieCharts/paymentSummaryChart.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Chart } from "react-google-charts";
+import { useEffect, useState } from "react";
+import axios from "../../../../Uri";
 
-const data = [
-  ["Task", "Hours per Day"],
-  ["PendingPayments", 11],
-  ["CompletedPayments", 2],
+var apidata = null
+var innerapidata = null
+var data = null;
 
-];
+
+
+
+
 
 const options = {
   title: "PaymentSummary",
@@ -21,14 +25,60 @@ const options = {
   ],
 };
 
-export default function PaymentSummaryChart() {
+export default function PaymentSummaryChart(props) {
+  console.log(props.buildingId)
+  const [completedPayments, setCompletedPayments] = useState(0);
+  const [pendingPayments, setPendingPayments] = useState(0);
+   const [innerapidata , setinnerapidata] = React.useState([])
+  useEffect(() => {
+
+    async function fetchData() {
+      await axios.get(
+        `guest/getPendingAndCompletedById/${props.buildingId}`
+      )
+        .then((res) => {
+
+
+
+          setCompletedPayments(res.data[0][0])
+          setPendingPayments(res.data[0][1])
+          console.log(completedPayments)
+          console.log(pendingPayments)
+
+
+
+        })
+    }
+    fetchData();
+  }, [props.buildingId]);
+
+
+  //console.log(apidata[0])
+
+
+
+
+  var data = [
+    ["Task", "Hours per Day"],
+    ["Completed Payments", completedPayments],
+    ["Pending Payments", pendingPayments],
+  ];
+
+
+
   return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"400px"}
-    />
+
+
+
+    <>
+      {data === null ? (<div></div>) : (<Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />)}
+
+    </>
   );
 }
